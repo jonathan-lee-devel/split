8443b9c3ec9a0971219dafd14064930b78ec336b
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-11-19 11:39:22 +0000

    Updated navbar

--- services/split-ui/src/app/components/lib/navbar/navbar.component.ts
+++ services/split-ui/src/app/components/lib/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
 import {Component} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatSlideToggleModule],
+  imports: [CommonModule, NgOptimizedImage, MatSlideToggleModule, RouterLink, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   public isDarkMode = true;
+  isLoading = false;
+  isLoggedIn = false;
 
   onThemeSwitchChange() {
     this.isDarkMode = !this.isDarkMode;
 
     document.body.setAttribute('data-theme', (this.isDarkMode) ? 'light' : 'dark');
+  }
+
+  doLogout() {
+
   }
 }
--- services/split-ui/src/app/components/pages/landing-page/landing-page.component.ts
+++ services/split-ui/src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
 
