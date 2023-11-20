64520789f55b110eb7bb3c74e22f83c21f7fabf0
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-11-20 18:32:35 +0000

    Added grunt and grunt-bump

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: [
+          'package.json',
+          'services/split-ui/package.json',
+          'services/api/package.json',
+          'services/split-micro-users/package/json',
+        ],
+        updateConfigs: [],
+        commit: false,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'upstream',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: '',
+        regExp: false
+      }
+    },
+  });
+};
