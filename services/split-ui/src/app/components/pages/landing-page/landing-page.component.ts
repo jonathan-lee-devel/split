2ff16d5198e92095001222cdd007db8c56c9077c
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-11-27 23:34:58 +0000

    Updated landing page

--- services/split-ui/src/app/components/pages/landing-page/landing-page.component.ts
+++ services/split-ui/src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,30 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {afterRender, Component, OnInit} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
+import {SyncService} from '../../../services/sync/sync.service';
+import {CookiesNoticeService} from '../../../services/cookies-notice/cookies-notice.service';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgOptimizedImage, ReactiveFormsModule, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  constructor(
+      private syncService: SyncService,
+      private cookiesNoticeService: CookiesNoticeService,
+  ) {
+    afterRender(() => {
+      this.syncService.sync();
+    });
+  }
 
+  ngOnInit() {
+    if (this.syncService.isClientSide()) {
+      this.cookiesNoticeService.triggerIfNotAccepted();
+    }
+  }
 }
