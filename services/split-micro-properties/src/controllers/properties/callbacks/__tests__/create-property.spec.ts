665d290dd30ea5f8c61a844570fa91aaebf3ec67
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-11 21:50:56 +0000

    Release v0.2.59 Removed card border-radius

--- services/split-micro-properties/src/controllers/properties/callbacks/__tests__/create-property.spec.ts
+++ services/split-micro-properties/src/controllers/properties/callbacks/__tests__/create-property.spec.ts
@@ -5,7 +5,8 @@ describe('Create Property Callback Unit Tests', () => {
   it('When make create property callback Then defined function', async () => {
     // @ts-ignore
     const createPropertyCallback = makeCreatePropertyCallback({}, () => {
-    }, {}, () => {});
+    }, // @ts-ignore
+    {}, () => {});
     expect(createPropertyCallback).toBeInstanceOf(Function);
   });
 });
