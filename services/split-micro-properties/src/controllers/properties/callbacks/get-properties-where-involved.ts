1a833583445397e4caaad9bb7a536e97d0ba61e4
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-18 03:36:09 +0000

    Inlined variable

--- services/split-micro-properties/src/controllers/properties/callbacks/get-properties-where-involved.ts
+++ services/split-micro-properties/src/controllers/properties/callbacks/get-properties-where-involved.ts
@@ -15,10 +15,12 @@ export const makeGetPropertiesWhereInvolvedCallback = (
     const requestingUserEmail = req.user.email;
     logger.info(`Request from <${requestingUserEmail}> to get properties where involved`);
 
-    return res.status(HttpStatus.OK).json((await Property.find({$or: [
-      {administratorEmails: requestingUserEmail},
-      {tenantEmails: requestingUserEmail},
-    ]}).exec())
-        .map((property) => property.toJSON({transform})),
+    return res.status(HttpStatus.OK).json(
+        (await Property.find({
+          $or: [
+            {administratorEmails: requestingUserEmail},
+            {tenantEmails: requestingUserEmail},
+          ]}).exec())
+            .map((property) => property.toJSON({transform})),
     );
   }) as any;
