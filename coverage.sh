283d6e40978f0c36c3a51ef9b9a96a00b0bf603a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-20 18:59:25 +0000

    Added all packages and services to coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf coverage
 mkdir -p coverage/workspaces
-for project in 'packages/split-auth' 'packages/split-constants' 'packages/split-observability' 'services/split-micro-users' 'services/split-micro-properties' 'services/split-micro-expenses'
+for project in 'packages/split-auth' 'packages/split-constants' 'packages/split-env' 'packages/split-http' 'packages/split-mail' 'packages/split-observability' 'packages/split-service-config' 'services/split-micro-users' 'services/split-micro-properties' 'services/split-micro-expenses' 'services/split-micro-mail'
 do
   if [ -f "$project/coverage/coverage-final.json" ]; then
     cp "$project"/coverage/coverage-final.json "$PWD"/coverage/workspaces/"$(basename $project)"-coverage-final.json
