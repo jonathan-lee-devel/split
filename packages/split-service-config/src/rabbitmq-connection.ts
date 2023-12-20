c81e2a7f0b020ae4004e0ee37c3bfdcffd3d7b20
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-20 07:09:29 +0000

    Updated statefulset volume policy, added retry to RabbitMQ connection

--- packages/split-service-config/src/rabbitmq-connection.ts
+++ packages/split-service-config/src/rabbitmq-connection.ts
@@ -1,5 +1,6 @@
 import {VALID_QUEUE_NAME} from '@split-common/split-constants';
 import amqp, {ConsumeMessage} from 'amqplib';
+import {Promise} from 'mongoose';
 import winston from 'winston';
 
 export type onMessageCallback = (message: ConsumeMessage | null) => void;
@@ -13,12 +14,18 @@ export class RabbitMQConnection {
     private connectionUrl: string,
   ) {}
 
-  public async connectQueue() {
+  public async connectQueue(retryCount?: number) {
+    retryCount = (retryCount) ? retryCount : 0;
     try {
       this.connection = await amqp.connect(this.connectionUrl);
       this.channel = await this.connection.createChannel();
     } catch (err) {
       this.logger.error(`Error initializing RabbitMQ connection: ${err}`);
+      if (retryCount === 0) {
+        this.logger.info(`Attempting to retry RabbitMQ connection`);
+        await this.delay(5000);
+        await this.connectQueue(1);
+      }
     }
   }
 
@@ -42,5 +49,9 @@ export class RabbitMQConnection {
 
   public getChannel() {
     return this.channel;
+  }
+
+  private delay(ms: number) {
+    return new Promise((resolve: () => void) => setTimeout(resolve, ms));
   }
 }
