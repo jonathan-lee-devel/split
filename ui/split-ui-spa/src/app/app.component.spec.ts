c00eb4164df190b232b7a43a8f9a0c5d7eea960e
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-22 03:25:33 +0000

    Removed default Angular unit test

--- ui/split-ui-spa/src/app/app.component.spec.ts
+++ ui/split-ui-spa/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -18,12 +18,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('split-ui-spa');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, split-ui-spa');
   });
 });
