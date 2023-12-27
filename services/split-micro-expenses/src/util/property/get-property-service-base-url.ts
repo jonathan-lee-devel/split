b57926c1d03e354c04c8dd45c489ccab6e2fb592
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-12-27 13:16:53 +0000

    Corrected getPropertiesServiceBaseUrlFromNodeEnv

--- services/split-micro-expenses/src/util/property/get-property-service-base-url.ts
+++ services/split-micro-expenses/src/util/property/get-property-service-base-url.ts
@@ -1,6 +1,6 @@
 export const getPropertiesServiceBaseUrlFromNodeEnv = (nodeEnv: string) => {
   if (nodeEnv === 'staging') {
-    return 'https://staging.api.split.direct/properties';
+    return 'https://api.staging.split.direct/properties';
   } else if (nodeEnv === 'production') {
     return 'https://api.split.direct/properties';
   } else {
