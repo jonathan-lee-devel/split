7f5b969f64220f203efc1fba504f4b7fd791499a
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-01-02 20:07:44 +0000

    Deleted stale files and tests

--- services/split-micro-users/src/controllers/auth/index.ts
+++ services/split-micro-users/src/controllers/auth/index.ts
@@ -1,13 +1,10 @@
 import {returnAnonymouslyBasedOnSafeParseResult} from '@split-common/split-http';
 
 import {makeConfirmRegistrationCallback} from './callbacks/confirm-registration';
-import {makeGetTokenFromTokenHoldCallback} from './callbacks/get-token-from-token-hold';
 import {makeMakeConfirmRegistrationEndpoint} from './endpoints/confirm-registration';
-import {makeMakeGetTokenFromTokenHoldEndpoint} from './endpoints/get-token-from-token-hold';
 import {ConfirmRegistrationRequestBodySchema, ConfirmRegistrationRequestQuerySchema} from './schemas/confirm-registration';
-import {GetTokenFromTokenHoldRequestBodySchema, GetTokenFromTokenHoldRequestQuerySchema} from './schemas/get-token-from-token-hold';
 import logger from '../../logger';
-import {RegistrationVerificationTokenModel, TokenHoldModel, UserModel} from '../../models';
+import {RegistrationVerificationTokenModel, UserModel} from '../../models';
 
 export const confirmRegistrationHandler = makeMakeConfirmRegistrationEndpoint(returnAnonymouslyBasedOnSafeParseResult)(
     ConfirmRegistrationRequestBodySchema,
@@ -17,10 +14,4 @@ export const confirmRegistrationHandler = makeMakeConfirmRegistrationEndpoint(re
         UserModel,
         RegistrationVerificationTokenModel,
     ),
-);
-
-export const getTokenFromTokenHoldHandler = makeMakeGetTokenFromTokenHoldEndpoint(returnAnonymouslyBasedOnSafeParseResult)(
-    GetTokenFromTokenHoldRequestBodySchema,
-    GetTokenFromTokenHoldRequestQuerySchema,
-    makeGetTokenFromTokenHoldCallback(TokenHoldModel),
 );
