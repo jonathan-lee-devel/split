0a086e6935f9fd74f26240f004b3954d39d1dbe9
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-01-02 21:17:29 +0000

    Made health check handler come from split-http

--- services/split-micro-properties/src/schemas/index.ts
+++ services/split-micro-properties/src/schemas/index.ts
@@ -3,7 +3,6 @@ export * from './create-property';
 export * from './delete-property-by-id';
 export * from './get-properties-where-involved';
 export * from './get-property-by-id';
-export * from './index-health-check';
 export * from './invite-tenant-to-property';
 export * from './toggle-property-administrator-status';
 export * from './toggle-property-tenant-status';
