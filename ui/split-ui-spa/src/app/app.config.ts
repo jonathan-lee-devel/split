b892f010b53b4d69d45eb371883e6e02f4b20c4e
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-01-04 22:55:01 +0000

    Deleted package-lock.json, corrected ESLint config

--- ui/split-ui-spa/src/app/app.config.ts
+++ ui/split-ui-spa/src/app/app.config.ts
@@ -1,15 +1,16 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
-import {DEFAULT_APP_PROVIDERS} from './default-app-providers';
 import {StoreModule} from '@ngrx/store';
+
 import {counterReducer} from './+state/counter.reducer';
+import {DEFAULT_APP_PROVIDERS} from './default-app-providers';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     ...DEFAULT_APP_PROVIDERS,
     importProvidersFrom(
-      StoreModule.forRoot({
-        count: counterReducer,
-      })),
+        StoreModule.forRoot({
+          count: counterReducer,
+        })),
   ],
 };
