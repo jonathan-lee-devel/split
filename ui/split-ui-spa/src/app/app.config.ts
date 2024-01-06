2f9aaed8569f6a5f73ae2f82bdbf3c8496ec0b1d
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-01-06 02:37:58 +0000

    Moved providers to DEFAULT_APP_PROVIDERS

--- ui/split-ui-spa/src/app/app.config.ts
+++ ui/split-ui-spa/src/app/app.config.ts
@@ -1,27 +1,10 @@
-import {ApplicationConfig, isDevMode} from '@angular/core';
-import {provideEffects} from '@ngrx/effects';
-import {provideState, provideStore} from '@ngrx/store';
-import {provideStoreDevtools} from '@ngrx/store-devtools';
+import {ApplicationConfig} from '@angular/core';
 
-import {counterReducer} from './+state/counter/counter.reducer';
-import {COUNT_FEATURE_NAME} from './+state/counter/counter.selector';
-import {PropertyEffects} from './components/pages/_properties/+state/property.effect';
-import {propertyReducer} from './components/pages/_properties/+state/property.reducer';
-import {PROPERTY_FEATURE_NAME} from './components/pages/_properties/+state/property.selector';
-import {ExpenseEffects} from './components/pages/_properties/_expenses/+state/expense.effect';
-import {expenseReducer} from './components/pages/_properties/_expenses/+state/expense.reducer';
-import {EXPENSE_FEATURE_NAME} from './components/pages/_properties/_expenses/+state/expense.selector';
 import {DEFAULT_APP_PROVIDERS} from './default-app-providers';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     ...DEFAULT_APP_PROVIDERS,
-    provideStore(),
-    provideState({name: COUNT_FEATURE_NAME, reducer: counterReducer}),
-    provideState({name: PROPERTY_FEATURE_NAME, reducer: propertyReducer}),
-    provideState({name: EXPENSE_FEATURE_NAME, reducer: expenseReducer}),
-    provideEffects([PropertyEffects, ExpenseEffects]),
-    provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()}),
   ],
 };
--- ui/split-ui-spa/src/app/default-app-providers.ts
+++ ui/split-ui-spa/src/app/default-app-providers.ts
@@ -1,21 +1,36 @@
 import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
+import {isDevMode} from '@angular/core';
 import {MAT_SNACK_BAR_DEFAULT_OPTIONS} from '@angular/material/snack-bar';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
-import {provideStore} from '@ngrx/store';
+import {provideEffects} from '@ngrx/effects';
+import {provideState, provideStore} from '@ngrx/store';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
+import {counterReducer} from './+state/counter/counter.reducer';
+import {COUNT_FEATURE_NAME} from './+state/counter/counter.selector';
 import {routes} from './app.routes';
+import {PropertyEffects} from './components/pages/_properties/+state/property.effect';
+import {propertyReducer} from './components/pages/_properties/+state/property.reducer';
+import {PROPERTY_FEATURE_NAME} from './components/pages/_properties/+state/property.selector';
+import {ExpenseEffects} from './components/pages/_properties/_expenses/+state/expense.effect';
+import {expenseReducer} from './components/pages/_properties/_expenses/+state/expense.reducer';
+import {EXPENSE_FEATURE_NAME} from './components/pages/_properties/_expenses/+state/expense.selector';
 import {AuthInterceptor} from './interceptors/auth/auth.interceptor';
 import {ErrorInterceptor} from './interceptors/error/error.interceptor';
 
 
 export const DEFAULT_APP_PROVIDERS = [
   provideStore(),
+  provideState({name: COUNT_FEATURE_NAME, reducer: counterReducer}),
+  provideState({name: PROPERTY_FEATURE_NAME, reducer: propertyReducer}),
+  provideState({name: EXPENSE_FEATURE_NAME, reducer: expenseReducer}),
+  provideEffects([PropertyEffects, ExpenseEffects]),
+  provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()}),
   provideRouter(routes),
   provideHttpClient(withFetch(), withInterceptorsFromDi()),
   {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
   {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
-  provideAnimations(),
   {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}},
   provideAnimations(),
 ];
